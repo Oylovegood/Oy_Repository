1da700ddc67cb950d7eab2b85e4e027d32d554d1
Oylovegood <409981341@qq.com>
2012-05-11 11:00:57 +0800

    #

---
+++ blog/protected/views/layouts/column1.php
@@ -0,0 +1,5 @@
+<?php $this->beginContent('//layouts/main'); ?>
+<div id="content" class="well">
+	<?php echo $content; ?>
+</div><!-- content -->
+<?php $this->endContent(); ?>
---
+++ blog/protected/views/post/index.php
@@ -0,0 +1,21 @@
+
+<?php
+$this->breadcrumbs=array(
+	'Posts',
+);
+
+$this->menu=array(
+	array('label'=>'Create Post', 'url'=>array('create')),
+	array('label'=>'Manage Post', 'url'=>array('admin')),
+);
+?>
+
+
+
+<h1>Posts</h1>
+<div class="well ouyangqiuhao1">
+<?php $this->widget('zii.widgets.CListView', array(
+	'dataProvider'=>$dataProvider,
+	'itemView'=>'_view',
+)); ?>
+</div>
